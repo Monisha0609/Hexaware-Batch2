ffe3376971c277cd4bbea96fe85803ce44f800a5
Monisha0609 <137783862+Monisha0609@users.noreply.github.com>
2025-04-09 15:28:40 +0530

    Update Dog.java

--- JavaCodingchallenge-petpals/entity/Dog.java
+++ JavaCodingchallenge-petpals/entity/Dog.java
@@ -1,5 +1,18 @@
 package entity;
+public class Dog extends Pet {
+    private String dogBreed;
 
-public class Dog {
+    public Dog(String name, int age, String breed, String dogBreed) {
+        super(name, age, breed);
+        this.dogBreed = dogBreed;
+    }
 
+    public String getDogBreed() { return dogBreed; }
+    public void setDogBreed(String dogBreed) { this.dogBreed = dogBreed; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", DogBreed='" + dogBreed + '\'';
+    }
 }
+
