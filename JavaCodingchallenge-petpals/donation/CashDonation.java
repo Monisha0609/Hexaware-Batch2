8f9d86f9b6668490fa52093f5264615280e59f87
Monisha0609 <137783862+Monisha0609@users.noreply.github.com>
2025-04-09 15:26:30 +0530

    Update CashDonation.java

--- JavaCodingchallenge-petpals/donation/CashDonation.java
+++ JavaCodingchallenge-petpals/donation/CashDonation.java
@@ -1,5 +1,23 @@
 package donation;
 
-public class CashDonation {
+import java.time.LocalDateTime;
 
+public class CashDonation extends Donation {
+    private LocalDateTime donationTime;
+
+    public CashDonation(String donorName, double amount, LocalDateTime donationTime) {
+        super(donorName, amount);
+        this.donationTime = donationTime;
+    }
+
+    public LocalDateTime getDonationTime() {
+        return donationTime;
+    }
+
+    @Override
+    public void recordDonation() {
+        System.out.println("Cash donation of $" + getAmount() + " by " + getDonorName() + " on " + donationTime);
+    }
 }
+
+
