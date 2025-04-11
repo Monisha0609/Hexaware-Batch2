4791dfdfee07ff72dce228bb15b1e3cdcaac6dcd
Monisha0609 <137783862+Monisha0609@users.noreply.github.com>
2025-04-11 13:26:39 +0530

    Add files via upload

---
+++ Assignment/exception/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package exception;
+
+
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ Assignment/exception/ParcelNotFoundException.java
@@ -0,0 +1,8 @@
+package exception;
+
+
+public class ParcelNotFoundException extends Exception {
+    public ParcelNotFoundException(String message) {
+        super(message);
+    }
+}
